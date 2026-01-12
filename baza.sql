58a0b665b70d6b4d2eedeab5b4ef2822bf422c20
Szymon <105111903+szymji99@users.noreply.github.com>
2026-01-12 23:58:58 +0100

    Add files via upload

---
+++ baza.sql
@@ -0,0 +1,24 @@
+--DROP TABLE IF EXISTS loginy;
+DROP TABLE IF EXISTS Notes;
+
+
+CREATE TABLE loginy(
+	ID INT PRIMARY KEY AUTO_INCREMENT
+	, login VARCHAR(30)
+	, password VARCHAR(30)
+	, email varchar(30)
+);
+
+INSERT INTO loginy (login,password,email) VALUES 
+	('admin','admin','szymji@gmail.com')
+	,('testuser','test','szymji@gmail.com');
+
+CREATE TABLE IF NOT EXISTS Notes (	
+	 NoteDay INT,
+	 NoteMonth INT,
+	 NoteYear INT,
+	 NoteText TINYTEXT,
+	 IDOwner INT
+
+	,FOREIGN KEY (IDOwner) REFERENCES loginy(ID) ON DELETE CASCADE
+);
